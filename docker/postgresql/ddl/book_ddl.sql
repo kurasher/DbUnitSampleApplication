2d9dc4a636632ec8bdf19a43cc1a9f6edbce25b0
kurasher <27tonokurasher07@gmail.com>
2023-02-25 03:30:29 +0900

    [mod] タイムゾーンの設定

--- docker/postgresql/ddl/book_ddl.sql
+++ docker/postgresql/ddl/book_ddl.sql
@@ -1,6 +1,8 @@
 create schema db_books;
 create table db_books.book (
-  id integer PRIMARY KEY,
-  title varchar(100),
-  author varchar(10)
+  id integer PRIMARY KEY NOT NULL,
+  title varchar(100) NOT NULL,
+  author varchar(10) NOT NULL,
+  created timestamp  DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated timestamp  DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
--- docker/postgresql/dml/book_dml.sql
+++ docker/postgresql/dml/book_dml.sql
@@ -1,5 +1,5 @@
 SET search_path = db_books;
 
-insert into book values (1, '営繕かるかや怪異譚', '小野不由美');
-insert into book values (2, '営繕かるかや怪異譚 その弐', '小野不由美');
-insert into book values (3, '営繕かるかや怪異譚 その参', '小野不由美');
+insert into book (id, title, author) values (1, '営繕かるかや怪異譚', '小野不由美');
+insert into book (id, title, author) values (2, '営繕かるかや怪異譚 その弐', '小野不由美');
+insert into book (id, title, author) values (3, '営繕かるかや怪異譚 その参', '小野不由美');
